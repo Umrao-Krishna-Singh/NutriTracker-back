8eadb8e435e3c7ade469d9d26cd36b70ac20ccc2
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-09-07 23:20:34 +0530

    utils

---
+++ src/utils/ip.util.ts
@@ -0,0 +1,21 @@
+/**
+ * @module utils/ip
+ */
+import { IncomingMessage } from 'http'
+import { FastifyRequest } from 'fastify'
+
+export const getIp = (request: FastifyRequest | IncomingMessage) => {
+  const _ = request as any
+
+  let ip: string =
+    _.headers['x-forwarded-for'] ||
+    _.ip ||
+    _.raw.connection.remoteAddress ||
+    _.raw.socket.remoteAddress ||
+    undefined
+
+  if (ip && ip.split(',').length > 0) {
+    ip = ip.split(',')[0]
+  }
+  return ip
+}
