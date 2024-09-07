bc76abe1754e9d967f1d7a87b0aff8ef1b9ee90d
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-09-07 23:22:55 +0530

    request logging interceptor and response transform interceptors

---
+++ src/common/interceptors/res-transform.interceptor.ts
@@ -0,0 +1,28 @@
+import {
+    Injectable,
+    NestInterceptor,
+    ExecutionContext,
+    CallHandler,
+    // Logger,
+} from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+
+interface Response<T> {
+    statusCode: number
+    message: string
+    data?: T
+}
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
+    // private readonly logger = new Logger(TransformInterceptor.name)
+    intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+        return next.handle().pipe(
+            map((data) => {
+                if (!data) return { statusCode: 200, message: 'Success' }
+                else return { statusCode: 200, message: 'Success', data }
+            }),
+        )
+    }
+}
