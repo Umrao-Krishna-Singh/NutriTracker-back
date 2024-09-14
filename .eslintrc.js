760fd68e118b37179028834592d800b3c5c65587
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-09-14 14:18:01 +0530

    finally got eslint working right. Packages changed, replaced vite-node with tsx. npmrc changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
 
     ignorePatterns: ['.eslintrc.js'],
     rules: {
-        endOfLine: ['error', 'auto'],
+        'eol-last': ['error', 'always'],
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
