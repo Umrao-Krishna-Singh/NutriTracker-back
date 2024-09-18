bfecac18f115e02fb6d5c5a4fafb208bceeabfa6
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-09-18 14:46:20 +0530

    search module controller

---
+++ src/modules/search/search.controller.ts
@@ -0,0 +1,24 @@
+import { Get, UsePipes, Query, Logger } from '@nestjs/common'
+import { SearchService } from './search.service'
+import { ApiController } from '@src/common/decorators/api-controller.decorator'
+import { FoodSearchDto, FoodSearchResponseDto } from './search.dto'
+import { GetFoodSearchValidationPipe } from './search.pipe'
+
+@ApiController('search')
+export class SearchController {
+    private readonly logger: Logger
+
+    constructor(private readonly searchService: SearchService) {
+        this.logger = new Logger(SearchController.name)
+    }
+
+    @Get('/food')
+    @UsePipes(new GetFoodSearchValidationPipe())
+    async getFoodDetails(
+        @Query() foodSearchDto: FoodSearchDto,
+    ): Promise<FoodSearchResponseDto | null> {
+        const data = await this.searchService.getFoodDetails(foodSearchDto.search)
+
+        return data
+    }
+}
