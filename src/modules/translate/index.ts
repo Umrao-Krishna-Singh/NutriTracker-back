5998382c8a223c0d4c72f367c1204f5e1070a3d2
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-10-08 23:28:40 +0530

    test translate api from google

--- src/modules/translate/index.ts
+++ src/modules/translate/index.ts
@@ -1,7 +1,17 @@
 // Imports the Google Cloud client library
 import { v3 } from '@google-cloud/translate'
+
 const { TranslationServiceClient } = v3
-const contents = ['me', 'mine', 'what']
+const contents = [
+    'chocolate milk',
+    'no',
+    'mcdouble (mcdonalds)',
+    `what's up`,
+    'Fudgesicle',
+]
+import { appendFileSync } from 'fs'
+import { join } from 'path'
+
 /**
  *  Optional. The format of the source text, for example, "text/html",
  *   "text/plain". If left blank, the MIME type defaults to "text/html".
@@ -21,20 +31,7 @@ const parent = 'projects/nutritracker-translate'
  *  Optional. Transliteration to be applied.
  */
 // const transliterationConfig = {}
-/**
- *  Optional. The labels with user-defined metadata for the request.
- *  Label keys and values can be no longer than 63 characters
- *  (Unicode codepoints), can only contain lowercase letters, numeric
- *  characters, underscores and dashes. International characters are allowed.
- *  Label values are optional. Label keys must start with a letter.
- *  See https://cloud.google.com/translate/docs/advanced/labels for more
- *  information.
- */
-// const labels = [1,2,3,4]
 
-// Imports the Translation library
-
-// Instantiates a client
 const translationClient = new TranslationServiceClient()
 
 async function callTranslateText() {
@@ -47,7 +44,12 @@ async function callTranslateText() {
 
     // Run request
     const [{ translations }] = await translationClient.translateText(request)
-    console.log(translations?.map((item) => item.translatedText))
+    const translatedText: string[] | undefined = translations
+        ?.filter((item) => !!item.translatedText)
+        ?.map((item) => item.translatedText!)
+
+    if (!translatedText?.length) console.error('translated text empty')
+    else appendFileSync(join(__dirname, 'tlt.txt'), `\n` + JSON.stringify(translatedText))
 }
 
 callTranslateText()
