6d47f386fe932bb91c6c04bb176fc9808296695f
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-10-29 17:38:45 +0530

    prettier

--- src/database/db.module.ts
+++ src/database/db.module.ts
@@ -2,8 +2,8 @@ import { Module, Global } from '@nestjs/common'
 import { DatabaseService } from './db.service'
 
 @Module({
-  providers: [DatabaseService],
-  exports: [DatabaseService],
+    providers: [DatabaseService],
+    exports: [DatabaseService],
 })
 @Global()
 export class DatabaseModule {}
