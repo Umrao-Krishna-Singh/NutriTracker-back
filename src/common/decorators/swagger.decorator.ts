9147d4d618a36e3295d478cb36b9e4ea4ccac079
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-11-09 16:54:31 +0530

    swagger decorator

--- src/common/decorators/swagger.decorator.ts
+++ src/common/decorators/swagger.decorator.ts
@@ -1,16 +1,80 @@
 import { applyDecorators } from '@nestjs/common'
 import {
+    ApiProperty,
     ApiUnauthorizedResponse,
     ApiForbiddenResponse,
     ApiBadRequestResponse,
     ApiInternalServerErrorResponse,
+    ApiResponseNoStatusOptions,
+    ApiOkResponse,
 } from '@nestjs/swagger'
+
+class Unauth {
+    @ApiProperty({ example: false })
+    status!: boolean
+    @ApiProperty({ example: 401 })
+    code!: number
+    @ApiProperty({ example: 'Invalid User' })
+    message!: string
+}
+
+class BadReq {
+    @ApiProperty({ example: false })
+    status!: boolean
+    @ApiProperty({ example: 400 })
+    code!: number
+    @ApiProperty({ example: 'Bad Request' })
+    message!: string
+}
+
+class Forbidden {
+    @ApiProperty({ example: false })
+    status!: boolean
+    @ApiProperty({ example: 403 })
+    code!: number
+    @ApiProperty({ example: 'Unauthorized Request' })
+    message!: string
+}
+
+class InternalError {
+    @ApiProperty({ example: false })
+    status!: boolean
+    @ApiProperty({ example: 500 })
+    code!: number
+    @ApiProperty({ example: 'Something went wrong' })
+    message!: string
+}
 
 export function ApiUnsuccessfulResponse() {
     return applyDecorators(
-        ApiUnauthorizedResponse(),
-        ApiBadRequestResponse(),
-        ApiForbiddenResponse(),
-        ApiInternalServerErrorResponse(),
+        ApiBadRequestResponse({ type: BadReq, description: 'Validation Exception' }),
+        ApiUnauthorizedResponse({
+            type: Unauth,
+            description: 'Authentication Exception',
+        }),
+        ApiForbiddenResponse({ type: Forbidden, description: 'Authorization Exception' }),
+        ApiInternalServerErrorResponse({
+            type: InternalError,
+            description: 'Server Exception',
+        }),
+    )
+}
+
+export function ApiGeneralResponse(options?: ApiResponseNoStatusOptions) {
+    if (!options) options = { description: 'Success' }
+    else if (!options.description) options.description = 'Success'
+
+    return applyDecorators(
+        ApiOkResponse(options),
+        ApiBadRequestResponse({ type: BadReq, description: 'Validation Exception' }),
+        ApiUnauthorizedResponse({
+            type: Unauth,
+            description: 'Authentication Exception',
+        }),
+        ApiForbiddenResponse({ type: Forbidden, description: 'Authorization Exception' }),
+        ApiInternalServerErrorResponse({
+            type: InternalError,
+            description: 'Server Exception',
+        }),
     )
 }
