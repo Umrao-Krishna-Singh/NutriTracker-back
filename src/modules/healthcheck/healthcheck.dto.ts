4053c087a1a94d8d259d3ae55926e555bfda147d
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-11-10 14:41:47 +0530

    Healthcheck module import the common property for 200 response

--- src/modules/healthcheck/healthcheck.dto.ts
+++ src/modules/healthcheck/healthcheck.dto.ts
@@ -1,10 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger'
-
-export class HealthCheckDto {
-    @ApiProperty({ example: 200, description: 'Status code of response' })
-    statusCode!: number
-    @ApiProperty({ example: 'Success', description: 'Response message from the API' })
-    message!: string
+import { SuccessDto } from '@src/constants/swagger.constant'
+export class HealthCheckDto extends SuccessDto {
     @ApiProperty({ example: 'Success from backend!', description: 'Value of response' })
     data!: string
 }
