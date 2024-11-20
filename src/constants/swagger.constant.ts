5c8cb535cbee6f22bb3cac4b1e2000d6ad3e951e
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-11-20 16:57:50 +0530

    swagger pagination Dto constant

--- src/constants/swagger.constant.ts
+++ src/constants/swagger.constant.ts
@@ -6,3 +6,14 @@ export class SuccessDto {
     @ApiProperty({ example: 200, description: 'Response code' })
     code!: number
 }
+
+export class PaginateDto {
+    @ApiProperty({ example: true, description: 'Response status' })
+    status!: boolean
+    @ApiProperty({ example: 200, description: 'Response code' })
+    code!: number
+    @ApiProperty({ example: 1, description: 'Page number' })
+    page!: number
+    @ApiProperty({ example: 5, description: 'Total number of pages' })
+    pageCount!: number
+}
