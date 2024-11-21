7a521db7af7c9e756385a17bc23a827fa8c1b8f2
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-11-21 14:18:59 +0530

    import changes for swagger dto util

--- src/modules/healthcheck/healthcheck.dto.ts
+++ src/modules/healthcheck/healthcheck.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { SuccessDto } from '@src/constants/swagger.constant'
+import { SuccessDto } from '@src/utils/swagger.dtos.util'
 export class HealthCheckDto extends SuccessDto {
     @ApiProperty({ example: 'Success from backend!', description: 'Value of response' })
     data!: string
