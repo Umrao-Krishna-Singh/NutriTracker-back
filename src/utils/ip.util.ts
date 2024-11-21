e6cc40618386e5202860178584b7886aebdaf96c
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-11-21 16:44:10 +0530

    prettier

--- src/utils/ip.util.ts
+++ src/utils/ip.util.ts
@@ -5,17 +5,17 @@ import { IncomingMessage } from 'http'
 import { FastifyRequest } from 'fastify'
 
 export const getIp = (request: FastifyRequest | IncomingMessage) => {
-  const _ = request as any
+    const _ = request as any
 
-  let ip: string =
-    _.headers['x-forwarded-for'] ||
-    _.ip ||
-    _.raw.connection.remoteAddress ||
-    _.raw.socket.remoteAddress ||
-    undefined
+    let ip: string =
+        _.headers['x-forwarded-for'] ||
+        _.ip ||
+        _.raw.connection.remoteAddress ||
+        _.raw.socket.remoteAddress ||
+        undefined
 
-  if (ip && ip.split(',').length > 0) {
-    ip = ip.split(',')[0]
-  }
-  return ip
+    if (ip && ip.split(',').length > 0) {
+        ip = ip.split(',')[0]
+    }
+    return ip
 }
