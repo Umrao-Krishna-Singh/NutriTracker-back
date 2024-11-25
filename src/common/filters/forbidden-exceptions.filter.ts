f974fc081365a3dd6715e96472f6a913e4bd9ec8
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-11-25 23:36:54 +0530

    forbidden exception filter logging changes

--- src/common/filters/forbidden-exceptions.filter.ts
+++ src/common/filters/forbidden-exceptions.filter.ts
@@ -1,11 +1,23 @@
-import { ExceptionFilter, Catch, ArgumentsHost, ForbiddenException } from '@nestjs/common'
-import { FastifyReply } from 'fastify'
+import {
+    ExceptionFilter,
+    Catch,
+    ArgumentsHost,
+    ForbiddenException,
+    Logger,
+} from '@nestjs/common'
+import { FastifyRequest, FastifyReply } from 'fastify'
 
 @Catch(ForbiddenException)
 export class ForbiddenExceptionFilter implements ExceptionFilter {
+    private logger = new Logger(ForbiddenExceptionFilter.name)
+
     catch(exception: ForbiddenException, host: ArgumentsHost) {
         const ctx = host.switchToHttp()
         const response = ctx.getResponse<FastifyReply>()
+        const request = ctx.getRequest<FastifyRequest>()
+
+        const content = `${request.method} -> ${request.url}`
+        this.logger.verbose(`--- ResponseError：${content}`)
 
         response.status(exception.getStatus()).type('application/json').send({
             status: false,
