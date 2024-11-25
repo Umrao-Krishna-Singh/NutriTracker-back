47661cdb26e0612e470333a2d9255cf10935c9cc
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-11-25 23:36:21 +0530

    request logging middleware - comment refactor

--- src/common/middlewares/req-logging-middleware.ts
+++ src/common/middlewares/req-logging-middleware.ts
@@ -1,12 +1,12 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
-@Injectable()
 /**
  * @Module - Since Guard is activated before interceptor, so the request logging interceptor
  *  will not trigger in case of guard failure. This additional log will
  *  ensure that all requests get logged.
  */
+@Injectable()
 export class LoggerMiddleware implements NestMiddleware {
     private logger: Logger = new Logger(LoggerMiddleware.name)
 
