6db4939d2e5114a58e19eefd096ee5cec794f927
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-11-25 23:37:05 +0530

    unauthorized exception filter logging changes

--- src/common/filters/unauthorized-exceptions.filter.ts
+++ src/common/filters/unauthorized-exceptions.filter.ts
@@ -3,14 +3,21 @@ import {
     Catch,
     ArgumentsHost,
     UnauthorizedException,
+    Logger,
 } from '@nestjs/common'
-import { FastifyReply } from 'fastify'
+import { FastifyRequest, FastifyReply } from 'fastify'
 
 @Catch(UnauthorizedException)
 export class UnauthorizedExceptionFilter implements ExceptionFilter {
+    private logger = new Logger(UnauthorizedExceptionFilter.name)
+
     catch(exception: UnauthorizedException, host: ArgumentsHost) {
         const ctx = host.switchToHttp()
         const response = ctx.getResponse<FastifyReply>()
+        const request = ctx.getRequest<FastifyRequest>()
+
+        const content = `${request.method} -> ${request.url}`
+        this.logger.verbose(`--- ResponseError：${content}`)
 
         response.status(exception.getStatus()).type('application/json').send({
             status: false,
