456e1a9ab37268de325564742850b7817fdf1145
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-11-27 14:50:12 +0530

    initial migration from mysql to postgres

---
+++ prisma/migrations/20241127091716_initial_postgres_migration/migration.sql
@@ -0,0 +1,214 @@
+-- CreateEnum
+CREATE TYPE "Roles" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "Units" AS ENUM ('G', 'IU', 'KCAL', 'MCG_RE', 'MG', 'MG_ATE', 'MG_GAE', 'PH', 'SP_GR', 'UG', 'UMOL_TE', 'kJ');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" BIGSERIAL NOT NULL,
+    "pass_hash" VARCHAR(100) NOT NULL,
+    "first_name" VARCHAR(126) NOT NULL,
+    "last_name" VARCHAR(126) NOT NULL,
+    "is_verified" BOOLEAN NOT NULL DEFAULT false,
+    "weight" INTEGER,
+    "goal_weight" INTEGER,
+    "height" INTEGER,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserRole" (
+    "id" BIGSERIAL NOT NULL,
+    "user_id" BIGINT NOT NULL,
+    "role" "Roles" NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "UserRole_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserAuthToken" (
+    "id" BIGSERIAL NOT NULL,
+    "user_id" BIGINT NOT NULL,
+    "token" VARCHAR(500) NOT NULL,
+    "issued_at" TIMESTAMP(3) NOT NULL,
+    "expire_at" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UserAuthToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserRefreshToken" (
+    "id" BIGSERIAL NOT NULL,
+    "user_id" BIGINT NOT NULL,
+    "token" VARCHAR(500) NOT NULL,
+    "issued_at" TIMESTAMP(3) NOT NULL,
+    "expire_at" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UserRefreshToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserEmail" (
+    "id" BIGSERIAL NOT NULL,
+    "user_id" BIGINT,
+    "is_created" BOOLEAN NOT NULL DEFAULT false,
+    "email" VARCHAR(256) NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "UserEmail_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserEmailOtp" (
+    "id" BIGSERIAL NOT NULL,
+    "email_id" BIGINT NOT NULL,
+    "otp" INTEGER NOT NULL,
+    "expire_at" TIMESTAMP(3) NOT NULL,
+    "resend_expire_at" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UserEmailOtp_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Food" (
+    "id" BIGSERIAL NOT NULL,
+    "fdc_id" BIGINT,
+    "description" VARCHAR(500) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Food_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "DuplicateFood" (
+    "id" BIGSERIAL NOT NULL,
+    "description" VARCHAR(500) NOT NULL,
+    "fdc_id" BIGINT,
+    "type" SMALLINT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "DuplicateFood_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Nutrient" (
+    "id" BIGSERIAL NOT NULL,
+    "fdc_nutrient_id" BIGINT NOT NULL,
+    "name" VARCHAR(200) NOT NULL,
+    "unit_name" "Units",
+    "nutrient_nbr" BIGINT,
+    "rank" BIGINT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Nutrient_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FoodNutrient" (
+    "id" BIGSERIAL NOT NULL,
+    "food_id" BIGINT NOT NULL,
+    "nutrient_id" BIGINT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "FoodNutrient_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" BIGSERIAL NOT NULL,
+    "tag_name" VARCHAR(100) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FoodTag" (
+    "id" BIGSERIAL NOT NULL,
+    "food_id" BIGINT NOT NULL,
+    "tag_id" BIGINT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "FoodTag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserRole_user_id_role_status_key" ON "UserRole"("user_id", "role", "status");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserAuthToken_token_key" ON "UserAuthToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserRefreshToken_token_key" ON "UserRefreshToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserEmail_user_id_key" ON "UserEmail"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserEmail_email_key" ON "UserEmail"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserEmailOtp_email_id_key" ON "UserEmailOtp"("email_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Food_fdc_id_key" ON "Food"("fdc_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Food_description_key" ON "Food"("description");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Nutrient_fdc_nutrient_id_key" ON "Nutrient"("fdc_nutrient_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Nutrient_nutrient_nbr_key" ON "Nutrient"("nutrient_nbr");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FoodNutrient_food_id_nutrient_id_key" ON "FoodNutrient"("food_id", "nutrient_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tag_tag_name_key" ON "Tag"("tag_name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FoodTag_food_id_tag_id_key" ON "FoodTag"("food_id", "tag_id");
+
+-- AddForeignKey
+ALTER TABLE "UserRole" ADD CONSTRAINT "UserRole_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserAuthToken" ADD CONSTRAINT "UserAuthToken_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserRefreshToken" ADD CONSTRAINT "UserRefreshToken_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserEmail" ADD CONSTRAINT "UserEmail_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserEmailOtp" ADD CONSTRAINT "UserEmailOtp_email_id_fkey" FOREIGN KEY ("email_id") REFERENCES "UserEmail"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FoodNutrient" ADD CONSTRAINT "FoodNutrient_food_id_fkey" FOREIGN KEY ("food_id") REFERENCES "Food"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FoodNutrient" ADD CONSTRAINT "FoodNutrient_nutrient_id_fkey" FOREIGN KEY ("nutrient_id") REFERENCES "Nutrient"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FoodTag" ADD CONSTRAINT "FoodTag_food_id_fkey" FOREIGN KEY ("food_id") REFERENCES "Food"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FoodTag" ADD CONSTRAINT "FoodTag_tag_id_fkey" FOREIGN KEY ("tag_id") REFERENCES "Tag"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
