9e84d39c08504927539a15410d88d569c3c2a2be
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-12-01 03:06:14 +0530

    don't wait for the email being sent

--- src/utils/send-otp.util.ts
+++ src/utils/send-otp.util.ts
@@ -5,5 +5,5 @@ export const sendOtpMail = async (to: string, otp: number) => {
     const expiry = 10
     const htmlPath = otpVerEmail(otp, expiry)
 
-    await sendMail(to, 'Verify your email', null, htmlPath, null)
+    sendMail(to, 'Verify your email', null, htmlPath, null)
 }
